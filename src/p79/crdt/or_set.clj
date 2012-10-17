4e7e5ec0769defeb9b27874146bcf442ffb95f2b
Chas Emerick <chas@cemerick.com>
2012-10-17 02:30:37 -0400

    Add metadata slot to OR-Set

--- src/p79/crdt/or_set.clj
+++ src/p79/crdt/or_set.clj
@@ -6,47 +6,56 @@
   []
   (java.util.UUID/randomUUID))
 
-(deftype ObservedRemoveSet [adds removes size]
+(deftype ObservedRemoveSet [adds removes size metadata]
   clojure.lang.IPersistentCollection
   (count [this] size)
   (empty [this]
-    (ObservedRemoveSet. {} {} 0))
+    (ObservedRemoveSet. {} {} 0 nil))
   (equiv [this other]
     (.equals this other))
   (cons [this v]
     (ObservedRemoveSet.
       (update-in adds [v] (fnil conj #{}) (tag))
       removes
-      (inc size)))
+      (inc size)
+      metadata))
   
   Object
   (hashCode [this]
     (hash (set (seq this))))
   (equals [this other]
     (or (identical? this other)
-        (and (instance? java.util.Set other)
+        (and (set? other)
              (and (= (count this) (count other))
-                  (every? #(.contains this %) (seq other))))))
+                  (every? #(contains? this %) (seq other))))))
   
   clojure.lang.Seqable
   (seq [this]
-    (filter #(.contains this %) (keys adds)))
+    (filter #(contains? this %) (keys adds)))
+  ^:clj java.lang.Iterable
+  ^:clj (iterator [this] (.iterator (seq this)))
+  ; only need to be instanceof Set to make equality work
+  ; can't forsee a reason to implement more of the interface
+  ^:clj java.util.Set
+  ^:clj (size [this] size)
   
   clojure.lang.IPersistentSet
   (contains [this v]
     (boolean (seq (set/difference (adds v) (removes v)))))
   (get [this v]
-    (and (.contains this v) v))
+    (and (contains? this v) v))
   (disjoin [this v]
-    (ObservedRemoveSet.
-      ;; TODO just let this work happen in join?
-      (dissoc adds v)
-      (update-in removes [v] (fnil into #{}) (adds v))
-      (dec size)))
+    (if-not (contains? this v)
+      this
+      (ObservedRemoveSet.
+        (dissoc adds v)
+        (update-in removes [v] (fnil into #{}) (adds v))
+        (dec size)
+        metadata)))
   
   java.io.Serializable
   clojure.lang.IFn
-  (invoke [this k] (.get this k))
+  (invoke [this k] (get this k))
   
   p79.crdt.Joinable
   (join [this other]
@@ -61,8 +70,9 @@
                        :default (assoc adds v pruned))))
                  (merge-with set/union (.adds this) (.adds other))
                  (merge-with set/union (.removes this) (.removes other)))]
-      (ObservedRemoveSet. adds {} (count adds)))))
+      (ObservedRemoveSet. adds {} (count adds) metadata))))
 
+^:clj
 (defmethod print-method ObservedRemoveSet
   [^ObservedRemoveSet o ^java.io.Writer w]
   (.write w "#")
@@ -77,14 +87,5 @@
   "Returns a new Observed-Remove Set containing the provided initial values."
   [& vals]
   (let [adds (zipmap vals (map hash-set (repeatedly tag)))]
-    (ObservedRemoveSet. adds {} (count adds))))
+    (ObservedRemoveSet. adds {} (count adds) nil)))
 
-(comment
-
-(def a (create 1 2 3 4 5))
-(def b (disj a 2))
-(def c (disj b 4))
-(def d (conj c 6))
-(def e (conj a 6))
-(def f (disj b 4))
-(p79.crdt/join e d))
