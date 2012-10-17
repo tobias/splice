f38372750e29705fa16d98b5f2c26b0bb084801b
Chas Emerick <chas@cemerick.com>
2012-10-17 02:30:53 -0400

    CmRDT protocol + docs

--- src/p79/crdt.clj
+++ src/p79/crdt.clj
@@ -1,4 +1,12 @@
 (ns p79.crdt)
 
 (defprotocol Joinable
-  (join [a b]))
+  (join [a b]
+    "Joins two CvRDTs, merging the provided states, potentially into a
+new lower upper bound."))
+
+(defprotocol CmRDT
+  (update [this operation arguments]
+    "Applies the given [operation] to a CmRDT using the supplied [arguments].
+Returns a new value, the metadata of which may contain a set of operations
+and arguments to propagate to other replicas of the CmRDT."))
