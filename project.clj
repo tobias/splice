13a027e3fb16bae1f382134a8a3bd922b61d0074
Chas Emerick <chas@cemerick.com>
2013-11-22 12:24:38 -0500

    restore project.clj after repo cleanse

---
+++ project.clj
@@ -0,0 +1,53 @@
+(defproject port79/crdt "0.0.1-SNAPSHOT"
+  :description ""
+  :url ""
+  :license "proprietary"
+  :resource-paths ["resources"]
+  :source-paths ["src/clj" "src/cljs"]
+  :test-paths ["test/cljx" "test/cljs" "test/clj" "target/test-classes"]
+  :min-lein-version "2.0.0"
+  :dependencies [[org.clojure/clojure "1.6.0-alpha1"]
+                 [org.clojure/core.match "0.2.0"]
+                 [org.clojure/core.incubator "0.1.1"]
+                 [org.clojure/math.combinatorics "0.0.4"]
+                 [com.cemerick/cljs-sanity "0.0.1"]]
+  :test-selectors {:default (complement :integration)
+                   :integration :integration
+                   :all (constantly true)}
+  
+  :cljx {:builds [{:source-paths ["src/cljx"]
+                   :output-path "target/classes"
+                   :rules :clj}
+                  
+                  {:source-paths ["test/cljx"]
+                   :output-path "target/test-classes"
+                   :rules :clj}
+                  
+                  {:source-paths ["src/cljx"]
+                   :output-path "target/classes"
+                   :rules :cljs}
+                  
+                  {:source-paths ["test/cljx"]
+                   :output-path "target/test-classes"
+                   :rules :cljs}]}
+  
+  :cljsbuild {:test-commands {"phantom" ["phantomjs" :runner "target/testable.js"]}
+              :builds [{:source-paths ["src/cljs" "target/classes" "target/test-classes"]
+                        :compiler {:output-to "target/testable.js"
+                                   :optimizations :whitespace
+                                   :pretty-print true}}]}
+  
+  :profiles {:dev {:dependencies [[com.cemerick/pomegranate "0.0.13"
+                                   :exclusions [org.apache.httpcomponents/httpcore]]
+                                  [org.clojure/clojurescript "0.0-2067"]]
+                   :plugins [[lein-cljsbuild "1.0.0-alpha2"]
+                             [com.cemerick/clojurescript.test "0.2.1"]
+                             [com.cemerick/austin "0.1.3"]
+                             [s3-wagon-private "1.1.2"]
+                             [com.keminglabs/cljx "0.3.2-SNAPSHOT"]]
+                   :aliases {
+                              "sanity-check" ["with-profile" "sanity-check"
+                                              "do" "clean," "cljsc," "compile"]
+                              "cleantest" ["do" "clean," "cljx" "once,"
+                                           "test," "cljsbuild" "test"]}}
+             :sanity-check {:aot :all}})
